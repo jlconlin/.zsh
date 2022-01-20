b87871fcf45e826208c795858c9fd95c97149a85
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2022-01-20 16:32:11 -0700

    Adding pdf2png function for Macs.

--- macos.zsh
+++ macos.zsh
@@ -13,3 +13,10 @@ export EDITOR="$VIM_APP_DIR/MacVim.app/Contents/MacOS/Vim"
 alias vim=$EDITOR
 alias gvim="vim -g"
 
+function pdf2png(){
+  if [[ -z $2 ]]; then
+    sips -s format png $1 --out ${1/.pdf/.png}
+  else
+    sips -s format png ${1} --out ${2}
+  fi
+}
